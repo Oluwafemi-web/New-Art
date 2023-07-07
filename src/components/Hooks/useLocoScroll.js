d9e7aa8520605eba03b26eed35d2f470f5a24c70
Ogo-Ayorinde Shekinah <ogoayorindeshekinah@gmail.com>
2023-07-07 20:33:16 +0100

    Update useLocoScroll.js

--- src/components/Hooks/useLocoScroll.js
+++ src/components/Hooks/useLocoScroll.js
@@ -1,5 +1,5 @@
 import LocomotiveScroll from "locomotive-scroll";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 import { gsap } from "gsap";
@@ -7,40 +7,41 @@ import { gsap } from "gsap";
 import "locomotive-scroll/src/locomotive-scroll.scss";
 gsap.registerPlugin(ScrollTrigger);
 
-export default function useLocoScroll(start) {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (!start) return;
-      const scrollEl = document.querySelector(".smooth-scroll.page-loaded");
-      const scroll = new LocomotiveScroll({
-        el: scrollEl,
-        smooth: true,
-        multiplier: 0.5,
-        class: "is-reveal",
-        //   dataScrollSpeed: 0.5,
-        // Add more options as needed
-      });
-      console.log("started");
+const useLocoScroll = (start) => {
+  const startLocoScroll = useCallback(() => {
+    if (!start) return;
 
-      gsap.to(".horizontal-scroll", {
-        x: "-=500", // Continuous horizontal scrolling
-        scrollTrigger: {
-          trigger: ".horizontal-scroll",
-          start: "top center",
-          end: "bottom center",
-          scrub: true,
-          repeat: -1, // Repeat the animation indefinitely
-          onRepeat: (self) => self.invalidate(), // Invalidate the animation on each repeat
-        },
-      });
+    const scrollEl = document.querySelector(".smooth-scroll.page-loaded");
+    const scroll = new LocomotiveScroll({
+      el: scrollEl,
+      smooth: true,
+      multiplier: 0.5,
+      class: "is-reveal",
+      //   dataScrollSpeed: 0.5,
+      // Add more options as needed
+    });
+    console.log("started");
 
-      return () => {
-        scroll.destroy();
-      };
-    }, 2000);
+    gsap.to(".horizontal-scroll", {
+      x: "-=500", // Continuous horizontal scrolling
+      scrollTrigger: {
+        trigger: ".horizontal-scroll",
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+        repeat: -1, // Repeat the animation indefinitely
+        onRepeat: (self) => self.invalidate(), // Invalidate the animation on each repeat
+      },
+    });
 
-    return () => {
-      clearTimeout(timer);
+    const cleanup = () => {
+      scroll.destroy();
     };
+
+    return cleanup;
   }, [start]);
-}
+
+  useEffect(startLocoScroll, [startLocoScroll]);
+};
+
+export default useLocoScroll;
