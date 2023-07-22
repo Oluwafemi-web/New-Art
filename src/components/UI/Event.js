2a0044c1f3f63c11cdd5b24d02fa850e1a347bc4
Ogo-Ayorinde Shekinah <ogoayorindeshekinah@gmail.com>
2023-07-22 21:41:24 +0100

    Update Event.js

--- src/components/UI/Event.js
+++ src/components/UI/Event.js
@@ -2,14 +2,14 @@ import { useInView } from "react-intersection-observer";
 import { PortableText } from "@portabletext/react";
 
 export default function Event(props) {
-  let thresholdValue = (props.dataScroll + 3) * 0.2;
+  // let thresholdValue = (props.dataScroll + 3) * 0.2;
 
-  const { ref: myRef, inView: isVisible } = useInView({
-    trigger: "scroll", // Trigger the effect when scrolling
-    threshold: thresholdValue ? thresholdValue : 0.2, // Adjust the threshold value as per your requirement
-  });
-
-  const delay = props.index + props.dataScroll * 3;
+  const { ref: myRef, inView: isVisible } = useInView();
+  // {
+  //   trigger: "scroll", // Trigger the effect when scrolling
+  //   threshold: thresholdValue ? thresholdValue : 0.2, // Adjust the threshold value as per your requirement
+  // }
+  // const delay = props.index + props.dataScroll * 3;
 
   return (
     <div className="col-lg-4 col-md-6">
@@ -17,7 +17,7 @@ export default function Event(props) {
         ref={myRef}
         className={`exhibition-box ${isVisible ? "is-reveal" : ""}`}
         data-scroll
-        style={{ animationDelay: `${delay ? delay : 0}s` }} // Add animation delay based on the index
+        // style={{ animationDelay: `${delay ? delay : 0}s` }} // Add animation delay based on the index
       >
         <figure>
           <a href="#">
